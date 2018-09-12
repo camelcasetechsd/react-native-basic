becdcda8178996d06d636ea4c9d6fc468d40f039
Mohamed Mahdy <php.power.arts@gmail.com>
2018-09-12 17:05:49 +0200

    show companies by category id

--- components/CompanyList.js
+++ components/CompanyList.js
@@ -1,91 +1,81 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, Image, ScrollView } from 'react-native'
-import { createStackNavigator } from 'react-navigation';
+import React, {Component} from 'react'
+import {Text, View, StyleSheet, Image, ScrollView} from 'react-native'
+import {createStackNavigator} from 'react-navigation';
 
 import Company from './Company.js'
 import CompaniesData from './json/companies.json' ;
 
 
-
 class CompanyList extends Component {
-   state = {
-      names: CompaniesData
-   }
+    state = {
+        names: CompaniesData
+    }
 
-   //to be removed after using dynamic json data
-   buildExampleCompanyList() {
-   	  companies = [];
-   	  for (counter=0; counter<31; counter++) {
-   	  	companies.push({id: counter, name: 'Company' + counter})
-   	  }
-   	  return companies;
-   }
+    companyGrouping(companies, categoryId) {
 
-   companyGrouping(companies) {
-
-   	companyGroups = [];
-		for (var counter=0; counter<companies.length; counter=counter+2) {
-			displayCompanies = [];
-			displayCompanies.push(companies[counter]);
-			displayCompanies.push(companies[counter+1]);
-			companyGroups.push(displayCompanies);
-		}
-
-		return companyGroups;
-   }
-
-   companyGroupsRenderer(company) {
-   		if (company != undefined) {
-   			return  (<Company
-	            	uri={{uri: 'https://facebook.github.io/react/logo-og.png'}}
-	            	imagestyle={cstyles.image}
-	            	textstyle={cstyles.text}
-	            	name={company.name}
-                  navigation={this.props.navigation}
-	              />
-	            );
-   		}
-
-   		return null;
-
-   }
-
-   render() {
-      return (
-         <View>
-            {
-				this.companyGrouping(this.state.names).map((item, index) => (
-                  <View
-                     key = {item[0].id}
-                     style = {cstyles.container}
-                     >
-                     {this.companyGroupsRenderer(item[0])}
-                     {this.companyGroupsRenderer(item[1])}
-                  </View>
-               ))
+        let companyGroups = null;
+        companies.map(function (item) {
+            if (categoryId === item.id) {
+                companyGroups = item.companies;
             }
-         </View>
-      )
-   }
+        });
+
+        return companyGroups;
+    }
+
+    companyGroupsRenderer(company) {
+        if (company != undefined) {
+            return (<Company
+                    uri={{uri: company.image}}
+                    imagestyle={cstyles.image}
+                    textstyle={cstyles.text}
+                    name={company.name}
+                    navigation={this.props.navigation}
+                />
+            );
+        }
+
+        return null;
+
+    }
+
+    render() {
+        /* 2. Get the param, provide a fallback value if not available */
+        const {navigation} = this.props;
+        const categoryId = navigation.getParam('categoryId', 'NO-ID');
+        const companiesData = this.companyGrouping(this.state.names, categoryId);
+        return (
+            <View style={{flexDirection: 'row', flex: 1}}>
+                {
+                    companiesData.map((item, index) => (
+                        <View
+                            style={cstyles.container}
+                            key={item.id}
+                        >
+                            {this.companyGroupsRenderer(item)}
+                        </View>
+                    ))
+                }
+            </View>
+        )
+    }
 }
 
 export default CompanyList
 
-const cstyles = StyleSheet.create ({
-   container: {
-      padding: 30,
-      marginTop: 3,
-      backgroundColor: '#d9f9b1',
-      alignItems: 'center',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-   },
-   text: {
-      color: '#4f603c',
-      textAlign: 'center'
-   },
-   image: {
-   	  width: 100,
-   	  height: 100
-   }
+const cstyles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        padding: 30,
+        marginTop: 3,
+        backgroundColor: '#d9f9b1',
+    },
+    text: {
+        color: '#4f603c',
+        textAlign: 'center'
+    },
+    image: {
+        width: 100,
+        height: 100
+    }
 })
